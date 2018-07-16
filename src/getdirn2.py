c0d7f5d186053994ec7688a36f0f106d33a9590f
Nirmal <nirmal.iyer@gmail.com>
2018-07-16 15:20:28 +0200

    Fixed bugs in getdirn and added getdirn2
    Getdirn code had a few bugs which were fixed
    1. Order of multiplication of rotation matrices was inverted - for Rb2c and Ri2carr
    2. Ri2b was unnecessarily being flipped for yaw and pitch because of order
    multiplication switch. Fixed that and removed this.
    3. plxpc1 2 3 were wrongly computed in camera coordinates (as sign of the
    angles was not being accounted for). Now a separate function getvect1t2 added
    to do this.
    4. thetvra1 2 3 wrongly computed. By definition vzs to ra has inverted angles.
    Corrected this.
    
    Added getdirn2.py to compute ratios for all sources in srclist. getdirn.py
    is now just a sample code for the way computation is performed. The lightcurve
    fitting method is abandoned. Instead flux ratios (in two LAXPCs) are being used (computed in
    getdirn2.py). The plan is to have another getdirn3.py which can obtain location
    regions for given ratios.

--- src/getdirn2.py
+++ src/getdirn2.py
@@ -7,6 +7,32 @@ from scipy.integrate import quad
 from rotfunc import *
 
 folfits="../../"
+width = 6
+ht = width
+plt.rc('font', family='serif', serif='Times',size=10)
+plt.rc('text', usetex=True)
+plt.rc('xtick', labelsize=10)
+plt.rc('ytick', labelsize=10)
+plt.rc('axes', labelsize=12)
+plt.rc('legend',fontsize=10)
+fig = plt.figure('Burst1')
+fig.set_size_inches(width,ht)
+plt.axes().set_aspect('equal', 'datalim')
+plt.ylim(-29.6,-28.5)
+plt.xlim(265.75,267.0)
+plt.gca().invert_xaxis();
+plt.xlabel(r" $\alpha$ (RA)")
+plt.ylabel(r" $\delta$ (dec)")
+plt.grid()
+fig2 = plt.figure('Burst2')
+fig2.set_size_inches(width,ht)
+plt.axes().set_aspect('equal', 'datalim')
+plt.ylim(-29.6,-28.5)
+plt.xlim(265.75,267.0)
+plt.gca().invert_xaxis();
+plt.xlabel(r" $\alpha$ (RA)")
+plt.ylabel(r" $\delta$ (dec)")
+plt.grid()
 
 def effarea(lxpc,elo=3.0,ehi=15.0) :
   """
@@ -96,28 +122,6 @@ def getangle(pntvec,offsvec) :
     return ang1,ang2
 
 
-def fitlicu(time,A,tau,t0,ra,dec,Ri2carr,pntvec) :
-    """
-    Function to fit the lightcurve and get ra and dec of source
-    """
-    nsingle = int(time.size/3) # For each LAXPC
-    orig = A*np.exp(-(time[:nsingle] - t0)/tau) #Assuming time for all LAXPCs are same
-    offvec = iner2dc(ra,dec) # undecided on whether to keep ra/dec in deg or rad.
-    # does keeping in deg give more area to search for algo and make it easier to
-    # converge ?
-    thet1 = np.empty(nsingle) #In YZ plane perp. to x axis
-    thet2 = np.empty(nsingle) #In XZ plane perp. to y axis of ccsc
-    for ii in np.arange(nsingle) :
-      offvec_c = np.matrix(Ri2carr[ii])*offvec
-      ptpnt_c = np.matrix(Ri2carr[ii])*np.matrix(pntvec[ii]).T
-      thet1[ii],thet2[ii] = getangle(ptpnt_c.A1,offvec_c.A1)
-    
-    lx1 = orig*effarea(1)*collimfunc(thet1,thet2,1)
-    lx2 = orig*effarea(2)*collimfunc(thet1,thet2,2)
-    lx3 = orig*effarea(3)*collimfunc(thet1,thet2,3)
-    retarr = np.reshape((lx1,lx2,lx3),-1) # append to 1d array
-    return retarr,thet1,thet2
-
 def getvect1t2(thetx,thety,primary='x') :
     """
     Function to generate unit vector given angles t1 and t2
@@ -149,61 +153,47 @@ def getvect1t2(thetx,thety,primary='x') :
     vec = vec/np.linalg.norm(vec)
     return vec
 
+def getratio(srcra,srcdec,pntvec,Ri2c) :
+    offvec = iner2dc(srcra,srcdec)
+    ofc = Ri2c*offvec
+    pnc = Ri2c*np.matrix(pntvec).T
+    t1,t2 = getangle(pnc.A1,ofc.A1) 
+    lx1 = effarea(1)*np.abs(collimfunc(t1,t2,1))
+    lx2 = effarea(2)*np.abs(collimfunc(t1,t2,2))
+    return lx2/lx1
 
 
+listra,listdec = np.genfromtxt('srclist',usecols=(2,3),delimiter=',',unpack=True)
+srcnam = np.genfromtxt('srclist',usecols=(1),delimiter=',',dtype=str)
+
+offs = 0.017
 e1alph = 266.58787
 e1del = -28.72842
-saxalph = 266.76083
-saxdel = -28.88303
-grsalph = 266.25971
-grsdel = -28.91381
-ksalph = 266.21275
-ksdel = -29.35467
-e2alph = 266.52167
-e2del = -29.51481
-igralph = 266.82525
-igrdel = -27.34414
 
 alfcr = np.deg2rad(83.63)
 deltcr = np.deg2rad(22.01)
-alf1 = np.deg2rad(83.78)
-delt1 = np.deg2rad(22.01)
-alf2 = np.deg2rad(83.63)
-delt2 = np.deg2rad(22.08)
-alf3 = np.deg2rad(83.74)
-delt3 = np.deg2rad(22.03)
 
 rollcr = iner2dc(alfcr,deltcr)
-vec1 = iner2dc(alf1,delt1)
-vec2 = iner2dc(alf2,delt2)
-vec3 = iner2dc(alf3,delt3)
 
-#getting Ri2b_cr other way of using Roll Rotation avg of 4th and 6th orbit
 fth = fits.open(folfits + '4thorbit-AS1T01_052T01_9000000316lxp_level1.mkf') # RA Scan
 sth = fits.open(folfits + '6thorbit-AS1T01_052T01_9000000316lxp_level1.mkf') # Dec Scan
 fiid = np.arange(5120,10120) #indices for RA scan 4th orbit
 qtyf = np.abs(1.0*fth[1].data['Roll_RA'][fiid] - 83.63)
 idxf = np.where(qtyf == qtyf.min())[0][0] + fiid[0]
 qf = 1.0*fth[1].data['Q_SAT'][idxf] # multiplying 1.0 to get 64 bit precision.
-Rf = quat2dc(qf)
 vrs = np.array([0.0,0.0,1.0]) #x axis of collimator camera scan coordinate
-# (ccsc) = axis perpendicular to ra scan
-#vrs = Rf*np.matrix(vrsi).T
 
 siid = np.arange(7555,12940) #indices for DEC scan 6th orbit
 qtys = np.abs(1.0*sth[1].data['Roll_DEC'][siid] - 22.01)
 idxs = np.where(qtys == qtys.min())[0][0] + siid[0]
 rdid = idxs + 1069 # some random id in DEC scan
 qs = 1.0*fth[1].data['Q_SAT'][idxs]
-Rs = quat2dc(qs)
 vd1 = iner2dc(np.deg2rad(1.0*sth[1].data['Roll_RA'][idxs]),np.deg2rad(1.0*sth[1].data['Roll_DEC'][idxs]))
 vd2 = iner2dc(np.deg2rad(1.0*sth[1].data['Roll_RA'][idxs]),np.deg2rad(1.0*sth[1].data['Roll_DEC'][rdid]))
 #Manipulating the scan to be along orthogonal RA and DEC axes !
 vdsi = np.cross(vd1.A1,vd2.A1) # y axis of ccsc
 vdsi[2] = 0.0 #Manipulating the scan to be along orthogonal RA and DEC axes !
 vds = vdsi/np.linalg.norm(vdsi)
-#vds = Rs*np.matrix(vdsi).T Not shifting to body as scans are not perpendicular
-#in body coordinates !
 
 
 rrotn = 0.5*fth[1].data['Roll_ROT'][idxf] + 0.5*sth[1].data['Roll_ROT'][idxs]
@@ -227,147 +217,138 @@ Ri2b_cr = np.matrix((yawvec.A1,rollcr.A1,pitvec))
 Rb2c = Ri2c_cr*Ri2b_cr.T #camera to body conversion matrix - important and
 #fixed matrix irrespective of pointing
 
-vec1_b = Ri2b_cr*vec1
-vec2_b = Ri2b_cr*vec2
-vec3_b = Ri2b_cr*vec3
 
 hdllc = fits.open(folfits + 'Burst1-lxp2.lc')
 tstart1 = hdllc[1].header['TSTART']
 tend1 = hdllc[1].header['TSTOP']
 hdllc.close()
 
+hdllc2 = fits.open(folfits + 'Burst2-lxp2.lc')
+tstart2 = hdllc2[1].header['TSTART']
+tend2 = hdllc2[1].header['TSTOP']
+hdllc2.close()
+
+
 hdl = fits.open(folfits + 'AS1A03_116T01_9000001460lxp_level1-aux1.att')
 idsel = np.where((1.0*hdl[1].data['TIME'] >= tstart1) & \
         (1.0*hdl[1].data['TIME'] <= tend1))[0]
+idsel2 = np.where((1.0*hdl[1].data['TIME'] >= tstart2) & \
+        (1.0*hdl[1].data['TIME'] <= tend2))[0]
 quart = 1.0*hdl[1].data['Q_SAT'][idsel]
-
-l1bg = 440.0 # cts/s approx from looking at preburst licu
-l2bg = 350.0 
-l3bg = 225.0
-
-hdllc = fits.open(folfits + "test1.lc")
-tdel = hdllc[1].header['TIMEDEL']*86400.0 # in seconds
-time = 1.0*hdllc[1].data['TIME'] + (1.0*hdllc[1].header['TIMEZERI'] + \
-    hdllc[1].header['TIMEZERF'] - hdl[0].header['MJDREFI'] + 40000)*86400. # in sec since launch
-l1 = hdllc[1].data['RATE1']*tdel - l1bg*tdel
-e1 = hdllc[1].data['ERROR1']*tdel
-l2 = hdllc[1].data['RATE2']*tdel - l2bg*tdel
-e2 = hdllc[1].data['ERROR2']*tdel
-l3 = hdllc[1].data['RATE3']*tdel - l3bg*tdel
-e3 = hdllc[1].data['ERROR3']*tdel
+quart2 = 1.0*hdl[1].data['Q_SAT'][idsel2]
 tmatt = hdl[1].data['TIME'][idsel]
+tmatt2 = hdl[1].data['TIME'][idsel]
 
-t0 = time[0]
-tau = 4.6 # from fplot fit 
-#A = 660.0 - 1eval
-A = 960.0 
+Ri2b_b1 = quat2dc(quart[-1])
+Ri2b_b2 = quat2dc(quart2[-1])
+pntvec_b1 = Ri2b_b1[1] # roll vector
+pntvec_b2 = Ri2b_b2[1] # roll vector
+Ri2c_b1 = Rb2c*Ri2b_b1
+Ri2c_b2 = Rb2c*Ri2b_b2
 
+plt.figure('Burst1')
+plt.plot(listra,listdec,'ko',markersize=7)
+plt.plot(1.0*hdl[1].data['Roll_RA'][idsel][-1],1.0*hdl[1].data['Roll_DEC'][idsel][-1],'k^',markersize=8)
 
-tmarr = np.reshape((time,time,time),-1)
-nn = time.size
-Ri2carr = np.empty((time.size,3,3))
-pntvec = np.empty((time.size,3))
-for ii in np.arange(time.size) :
-    idd = np.where(tmatt <= (time[ii] + tdel))[0][-1] # taking last att value in that time window
-    Ri2b = quat2dc(quart[idd])
-#    Ri2b[0] = -Ri2b[0] # inverting yaw and pitch axes by 180 deg.
-#    Ri2b[2] = -Ri2b[2]
-    pntvec[ii] = Ri2b[1] # roll vector
-    Ri2carr[ii] = Rb2c*Ri2b
+plt.figure('Burst2')
+plt.plot(listra,listdec,'ko',markersize=7)
+plt.plot(1.0*hdl[1].data['Roll_RA'][idsel2][-1],1.0*hdl[1].data['Roll_DEC'][idsel2][-1],'k^',markersize=8)
 
-ltarr,t1,t2 = fitlicu(tmarr,A,tau,t0,np.deg2rad(e1alph),np.deg2rad(e1del),Ri2carr,pntvec)
-plt.figure('1E Licu')
-plt.errorbar(time,l1,yerr=e1,fmt='ro-',drawstyle='steps-mid')
-plt.errorbar(time,l2,yerr=e2,fmt='go-',drawstyle='steps-mid')
-plt.errorbar(time,l3,yerr=e3,fmt='bo-',drawstyle='steps-mid')
-plt.plot(time,ltarr[:nn],'r--',linewidth=1.9)
-plt.plot(time,ltarr[nn:2*nn],'g--',linewidth=1.9)
-plt.plot(time,ltarr[2*nn:],'b--',linewidth=1.9)
+ratvec = np.empty_like(listra)
+ratvec2 = np.empty_like(listra)
+for nam,i,lra,ldec in zip(srcnam,np.arange(listra.size),listra,listdec) :
+    ratvec[i] = getratio(np.deg2rad(lra),np.deg2rad(ldec),pntvec_b1,Ri2c_b1)
+    ratvec2[i] = getratio(np.deg2rad(lra),np.deg2rad(ldec),pntvec_b2,Ri2c_b2)
+    plt.figure('Burst1')
+    if (nam.find("1E") != -1) :
+        plt.text(lra - offs, ldec - 3*offs,nam)
+    else :
+        plt.text(lra - offs, ldec + offs,nam)
+    rtstr = "{0:.2f} : ".format(ratvec[i])
+    if (nam.find("1E") != -1) :
+        plt.text(lra + 4*offs, ldec - 3*offs,rtstr)
+    else :
+        plt.text(lra + 4*offs, ldec + offs,rtstr)
+    plt.figure('Burst2')
+    if (nam.find("1E") != -1) :
+        plt.text(lra - offs, ldec - 3*offs,nam)
+    else :
+        plt.text(lra - offs, ldec + offs,nam)
+    rtstr2 = "{0:0.2f} : ".format(ratvec2[i])
+    if (nam.find("1E") != -1) :
+        plt.text(lra + 4*offs, ldec - 3*offs,rtstr2)
+    else :
+        plt.text(lra + 4*offs, ldec + offs,rtstr2)
 
-plt.figure('Theta offsets')
-plt.axes().set_aspect('equal', 'datalim')
-plt.ylim(-29.6,-28.4)
-plt.xlim(265.8,267.2)
-plt.grid()
-plt.plot(e1alph,e1del,'bo',markersize=7)
-plt.plot([saxalph,grsalph,ksalph,e2alph,igralph],\
-        [saxdel,grsdel,ksdel,e2del,igrdel],'ko',markersize=7)
-ylo,yhi = plt.ylim()
-offs = (yhi - ylo)*0.05 # 10% of the range
-plt.text(266.1, e1del,'1E 1743.1-2843')
-plt.text(saxalph + 0.8*offs,saxdel,'SAX J1747.0-2853')
-plt.text(grsalph-2*offs,grsdel-offs,'GRS J1741.9-2853')
-plt.text(ksalph-2*offs,ksdel-offs,'KS J1741-293')
-plt.text(e2alph-2*offs,e2del-offs,'2E 1742.9-2929')
-plt.text(igralph-2*offs,igrdel-offs,'IGR J17473-272')
-cv1 = Ri2b.T*vec1_b
-cv2 = Ri2b.T*vec2_b
-cv3 = Ri2b.T*vec3_b
-alph1,del1 = unit2iner(cv1[0,0],cv1[1,0],cv1[2,0])
-alph2,del2 = unit2iner(cv2[0,0],cv2[1,0],cv2[2,0])
-alph3,del3 = unit2iner(cv3[0,0],cv3[1,0],cv3[2,0])
-plt.plot(np.rad2deg(alph1),np.rad2deg(del1),'r+',markersize=8.2)
-plt.plot(np.rad2deg(alph2),np.rad2deg(del2),'g+',markersize=8.2)
-plt.plot(np.rad2deg(alph3),np.rad2deg(del3),'b+',markersize=8.2)
-plt.plot(1.0*hdl[1].data['Roll_RA'][idsel][idd],1.0*hdl[1].data['Roll_DEC'][idsel][idd],'k^',markersize=10)
-
-Rc2i_pl = Ri2carr[-1].T
-cam_pntvec = Rc2i_pl*pvec_cr_c 
-campa,campd = unit2iner(cam_pntvec[0,0],cam_pntvec[1,0],cam_pntvec[2,0])
-plt.plot(np.rad2deg(campa),np.rad2deg(campd),'^',color='cyan',markersize=5.5)
-#RA Scan - xaxis, Dec Scan - yaxis
-xax = Rc2i_pl*np.matrix([1.0,0.0,0.0]).T
-yax = Rc2i_pl*np.matrix([0.0,1.0,0.0]).T
-pvec_c = Ri2carr[-1]*np.matrix(pntvec[-1]).T
+b1pvec_c = Ri2c_b1*np.matrix(pntvec_b1).T
+b2pvec_c = Ri2c_b1*np.matrix(pntvec_b1).T
 cf,dra1,ddec1 = collimfunc(1e-2,1e-2,1,full=True) # dummy values to ra and dec
 # done mainly for getting max vals for getting dra and ddec 
 cf,dra2,ddec2 = collimfunc(1e-2,1e-2,2,full=True)
 cf,dra3,ddec3 = collimfunc(1e-2,1e-2,3,full=True)
-thetvra1 = np.arctan2(pvec_c[1],pvec_c[2])[0,0] - dra1*np.pi/180. 
-# Remember this. V. V. Imp - Sense is flipped when measuring from vzs for ra !!
-thetvra2 = np.arctan2(pvec_c[1],pvec_c[2])[0,0] - dra2*np.pi/180.
-thetvra3 = np.arctan2(pvec_c[1],pvec_c[2])[0,0] - dra3*np.pi/180.
+b1thetvra1 = np.arctan2(b1pvec_c[1],b1pvec_c[2])[0,0] - dra1*np.pi/180. 
+b1thetvra2 = np.arctan2(b1pvec_c[1],b1pvec_c[2])[0,0] - dra2*np.pi/180.
+b1thetvra3 = np.arctan2(b1pvec_c[1],b1pvec_c[2])[0,0] - dra3*np.pi/180.
 
-thetvdec1 = np.arctan2(pvec_c[0],pvec_c[2])[0,0] + ddec1*np.pi/180.
-thetvdec2 = np.arctan2(pvec_c[0],pvec_c[2])[0,0] + ddec2*np.pi/180.
-thetvdec3 = np.arctan2(pvec_c[0],pvec_c[2])[0,0] + ddec3*np.pi/180.
+b1thetvdec1 = np.arctan2(b1pvec_c[0],b1pvec_c[2])[0,0] + ddec1*np.pi/180.
+b1thetvdec2 = np.arctan2(b1pvec_c[0],b1pvec_c[2])[0,0] + ddec2*np.pi/180.
+b1thetvdec3 = np.arctan2(b1pvec_c[0],b1pvec_c[2])[0,0] + ddec3*np.pi/180.
 
-plxpc1 = getvect1t2(thetvdec1,thetvra1)
-lxpc1 = Rc2i_pl*np.matrix(plxpc1).T
-alpc1,delpc1 = unit2iner(lxpc1[0,0],lxpc1[1,0],lxpc1[2,0])
-plt.plot(np.rad2deg(alpc1),np.rad2deg(delpc1),'rs',alpha=0.3)
+b2thetvra1 = np.arctan2(b2pvec_c[1],b2pvec_c[2])[0,0] - dra1*np.pi/180. 
+b2thetvra2 = np.arctan2(b2pvec_c[1],b2pvec_c[2])[0,0] - dra2*np.pi/180.
+b2thetvra3 = np.arctan2(b2pvec_c[1],b2pvec_c[2])[0,0] - dra3*np.pi/180.
 
-plxpc2 = getvect1t2(thetvdec2,thetvra2)
-lxpc2 = Rc2i_pl*np.matrix(plxpc2).T
-alpc2,delpc2 = unit2iner(lxpc2[0,0],lxpc2[1,0],lxpc2[2,0])
-plt.plot(np.rad2deg(alpc2),np.rad2deg(delpc2),'gs',alpha=0.3)
+b2thetvdec1 = np.arctan2(b2pvec_c[0],b2pvec_c[2])[0,0] + ddec1*np.pi/180.
+b2thetvdec2 = np.arctan2(b2pvec_c[0],b2pvec_c[2])[0,0] + ddec2*np.pi/180.
+b2thetvdec3 = np.arctan2(b2pvec_c[0],b2pvec_c[2])[0,0] + ddec3*np.pi/180.
 
-plxpc3 = getvect1t2(thetvdec3,thetvra3)
-lxpc3 = Rc2i_pl*np.matrix(plxpc3).T
-alpc3,delpc3 = unit2iner(lxpc3[0,0],lxpc3[1,0],lxpc3[2,0])
-plt.plot(np.rad2deg(alpc3),np.rad2deg(delpc3),'bs',alpha=0.3)
+plt.figure('Burst1')
+b1plxpc1 = getvect1t2(b1thetvdec1,b1thetvra1)
+b1lxpc1 = Ri2c_b1.T*np.matrix(b1plxpc1).T
+b1alpc1,b1delpc1 = unit2iner(b1lxpc1[0,0],b1lxpc1[1,0],b1lxpc1[2,0])
+plt.text(np.rad2deg(b1alpc1),np.rad2deg(b1delpc1),'L1',fontdict={'color':'r'})
+
+b1plxpc2 = getvect1t2(b1thetvdec2,b1thetvra2)
+b1lxpc2 = Ri2c_b1.T*np.matrix(b1plxpc2).T
+b1alpc2,b1delpc2 = unit2iner(b1lxpc2[0,0],b1lxpc2[1,0],b1lxpc2[2,0])
+plt.text(np.rad2deg(b1alpc2),np.rad2deg(b1delpc2),'L2',fontdict={'color':'g'})
+
+b1plxpc3 = getvect1t2(b1thetvdec3,b1thetvra3)
+b1lxpc3 = Ri2c_b1.T*np.matrix(b1plxpc3).T
+b1alpc3,b1delpc3 = unit2iner(b1lxpc3[0,0],b1lxpc3[1,0],b1lxpc3[2,0])
+plt.text(np.rad2deg(b1alpc3),np.rad2deg(b1delpc3),'L3',fontdict={'color':'m'})
+
+plt.figure('Burst2')
+b2plxpc1 = getvect1t2(b2thetvdec1,b2thetvra1)
+b2lxpc1 = Ri2c_b2.T*np.matrix(b2plxpc1).T
+b2alpc1,b2delpc1 = unit2iner(b2lxpc1[0,0],b2lxpc1[1,0],b2lxpc1[2,0])
+plt.text(np.rad2deg(b2alpc1),np.rad2deg(b2delpc1),'L1',fontdict={'color':'r'})
+
+b2plxpc2 = getvect1t2(b2thetvdec2,b2thetvra2)
+b2lxpc2 = Ri2c_b2.T*np.matrix(b2plxpc2).T
+b2alpc2,b2delpc2 = unit2iner(b2lxpc2[0,0],b2lxpc2[1,0],b2lxpc2[2,0])
+plt.text(np.rad2deg(b2alpc2),np.rad2deg(b2delpc2),'L2',fontdict={'color':'g'})
+
+b2plxpc3 = getvect1t2(b2thetvdec3,b2thetvra3)
+b2lxpc3 = Ri2c_b2.T*np.matrix(b2plxpc3).T
+b2alpc3,b2delpc3 = unit2iner(b2lxpc3[0,0],b2lxpc3[1,0],b2lxpc3[2,0])
+plt.text(np.rad2deg(b2alpc3),np.rad2deg(b2delpc3),'L3',fontdict={'color':'m'})
 
 
-#camera axis drawing
-dtetas = np.linspace(-0.01*np.pi/2,0.01*np.pi/2,101)
-#tetras = np.arctan2(pvec_c[2],pvec_c[1])[0,0] + dtetas
-tetras = np.arctan2(pvec_c[1],pvec_c[2])[0,0] + dtetas
-tetdecs = np.arctan2(pvec_c[0],pvec_c[2])[0,0] + dtetas
-#sgn = pvec_c[2]/np.abs(pvec_c[2])
-for ik in np.arange(dtetas.size) :
-    pax = getvect1t2(tetdecs[ik], tetras[int(dtetas.size/2)], primary='x')
-    #pax = getvect1t2(tetdecs[ik], 0.0, primary='x')
-    ipax = Rc2i_pl*np.matrix(pax).T
-    a,d = unit2iner(ipax[0,0],ipax[1,0],ipax[2,0])
-    plt.plot(np.rad2deg(a),np.rad2deg(d),'k.',markersize=2.2)
-    pay = getvect1t2(tetdecs[int(dtetas.size/2)], tetras[ik], primary='y')
-    #pay = getvect1t2(0.0, tetras[ik], primary='y')
-    ipay = Rc2i_pl*np.matrix(pay).T
-    a,d = unit2iner(ipay[0,0],ipay[1,0],ipay[2,0])
-    plt.plot(np.rad2deg(a),np.rad2deg(d),'m.',markersize=2.2)
+plt.figure('Burst1')
+plt.plot(e1alph,e1del,'bo',markersize=7)
+plt.figure('Burst2')
+plt.plot(e1alph,e1del,'bo',markersize=7)
+#np.savetxt("Ratio_b1",np.column_stack((listra,listdec,ratvec)),
+#        fmt='%3.2f    %3.2f    %3.5f') 
+#np.savetxt("Ratio_b2",np.column_stack((listra,listdec,ratvec2)),
+#        fmt='%3.2f    %3.2f    %3.5f') 
+
 
 plt.show()
 fth.close()
 sth.close()
-hdllc.close()
+#hdllc.close()
 hdl.close()
+fig.savefig('Burst1.pdf',format='pdf',dpi=500)
+fig2.savefig('Burst2.pdf',format='pdf',dpi=500)
